78708ce7129d6fb2807c0499816d3ee41239963c
Andrey <andy.gusev@list.ru>
2020-12-21 16:36:57 +0300

    Add method vectorization by passing the whole method to cubert

--- mmr/ds_vec.py
+++ mmr/ds_vec.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from argparse import ArgumentParser
+import argparse
 import json
 
 import numpy as np
@@ -7,39 +7,65 @@ import numpy as np
 from cubert_wrapper import CuBertWrapper
 
 
-def calc_vectors_dir(in_path: Path, out_path: Path, model: CuBertWrapper, max_len: int):
+def calc_vectors_dir(in_path: Path, out_path: Path, model: CuBertWrapper, one_run: bool, *,
+                     max_lines: int = -1):
     for tokens_file_path in in_path.glob('*.json'):
         with tokens_file_path.open() as tokens_file:
             tokens = json.load(tokens_file)
         rel_path = tokens_file_path.relative_to(in_path)
-        vecs_path = out_path / rel_path.with_suffix('.npy')
-        if vecs_path.is_file() or len(tokens) > max_len:
+        vectors_path = out_path / rel_path.with_suffix('.npy')
+        if vectors_path.is_file():
             continue
-        vecs_path.parent.mkdir(exist_ok=True, parents=True)
+        if one_run:
+            tokens = [sum(tokens, [])]
+        else:
+            if len(tokens) > max_lines:
+                continue
+        vectors_path.parent.mkdir(exist_ok=True, parents=True)
         token_vectors, line_vectors = model(tokens)
-        with open(vecs_path, 'wb') as out_file:
+        with open(vectors_path, 'wb') as out_file:
             np.savez(out_file, np.stack(line_vectors))
 
 
-def calc_vectors(in_path: Path, out_path: Path, model: CuBertWrapper, 
-                 max_class_len: int, max_method_len: int):
-    for project_path in in_path.iterdir():
+def calc_line_vectors(arguments: argparse.Namespace, model: CuBertWrapper):
+    for project_path in arguments.in_path.iterdir():
         project_name = project_path.name
-        project_vecs_path = out_path / project_name
-        calc_vectors_dir(project_path / 'methods', project_vecs_path / 'methods', model, max_method_len)
-        calc_vectors_dir(project_path / 'classes', project_vecs_path / 'classes', model, max_class_len)
-        calc_vectors_dir(project_path / 'classes_without_methods', project_vecs_path / 'classes_without_methods', model, max_class_len)
-                
-                
+        project_vectors_path = arguments.out_path / project_name
+        calc_vectors_dir(project_path / 'methods', project_vectors_path / 'methods', model, False,
+                         max_lines=arguments.max_method_len)
+        calc_vectors_dir(project_path / 'classes', project_vectors_path / 'classes', model, False,
+                         max_lines=arguments.max_class_len)
+        calc_vectors_dir(project_path / 'classes_without_methods', project_vectors_path / 'classes_without_methods',
+                         model, False, max_lines=arguments.max_class_len)
+
+
+def calc_method_vectors(arguments: argparse.Namespace, model: CuBertWrapper):
+    for project_path in arguments.in_path.iterdir():
+        project_name = project_path.name
+        project_vectors_path = arguments.out_path / project_name
+        calc_vectors_dir(project_path / 'methods', project_vectors_path / 'methods', model, True)
+
+
 if __name__ == '__main__':
-    arg_parser = ArgumentParser()
-    arg_parser.add_argument('in_path', type=Path)
-    arg_parser.add_argument('out_path', type=Path)
-    arg_parser.add_argument('model_path', type=Path)
-    arg_parser.add_argument('vocab_path', type=Path)
-    arg_parser.add_argument('--max_class_len', type=int, default=1000)
-    arg_parser.add_argument('--max_method_len', type=int, default=100)
+    arg_parser = argparse.ArgumentParser()
+    subparsers = arg_parser.add_subparsers()
+
+    per_line_parser = subparsers.add_parser('per_line')
+    per_line_parser.add_argument('in_path', type=Path)
+    per_line_parser.add_argument('out_path', type=Path)
+    per_line_parser.add_argument('model_path', type=Path)
+    per_line_parser.add_argument('vocab_path', type=Path)
+    per_line_parser.add_argument('--max_class_len', type=int, default=1000)
+    per_line_parser.add_argument('--max_method_len', type=int, default=100)
+    per_line_parser.set_defaults(func=calc_line_vectors)
+
+    whole_methods_parser = subparsers.add_parser('whole_methods')
+    whole_methods_parser.add_argument('in_path', type=Path)
+    whole_methods_parser.add_argument('out_path', type=Path)
+    whole_methods_parser.add_argument('model_path', type=Path)
+    whole_methods_parser.add_argument('vocab_path', type=Path)
+    whole_methods_parser.set_defaults(func=calc_method_vectors)
+
     args = arg_parser.parse_args()
-    
-    model = CuBertWrapper(args.model_path, args.vocab_path)
-    calc_vectors(args.in_path, args.out_path, model, args.max_class_len, args.max_method_len)
+    model_wrapper = CuBertWrapper(args.model_path, args.vocab_path)
+    args.func(args, model_wrapper)
